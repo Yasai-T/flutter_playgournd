c895ad8a2ca7cc85cc2c6a2b1f40ed143d7e42d2
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-08-07 15:52:18 +0900

    fix: duplicated app bar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,15 +10,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(child: RandomWords()),
-      ),
-    );
+    return MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
   }
 }
 
